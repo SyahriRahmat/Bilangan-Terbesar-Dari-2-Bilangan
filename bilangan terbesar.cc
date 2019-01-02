a51e0b4cf541afca683f88139cb4783dd2ca9473
SyahriRahmat <45659210+SyahriRahmat@users.noreply.github.com>
2019-01-03 05:43:20 +0700

    Add files via upload

---
+++ bilangan terbesar.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+int a,b;
+    cout<<"MENENTUKAN BILANGAN TERBESAR DARI 2 BILANGAN"<<endl;
+    cout<<"MASUKKAN BILANGAN 1 = ";
+    cin>>a;
+    cout<<"MASUKKAN BILANGAN 2 = ";
+    cin>>b;
+
+    if (a>b)
+        cout<<"Maka Bilangan Terbesar Adalah = "<<a<<endl;
+    else
+        cout<<"Maka Bilangan Terbesar Adalah = "<<b<<endl;
+}
